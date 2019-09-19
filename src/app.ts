cbcdddb1138bde7c672cb144e7728435461ecdc2
Haardik <haardik.haardik@securekey.com>
2019-09-19 10:38:07 -0400

    Setup TS App structure

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+
+class App {
+  public app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.config();
+  }
+
+  private config(): void {
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+  }
+}
+
+export default new App().app;
