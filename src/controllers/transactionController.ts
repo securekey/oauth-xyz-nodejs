a7855818443a3c0639fddb8bfd89133f8eeae0be
Haardik <haardik.haardik@securekey.com>
2019-09-23 14:26:03 -0400

    add transaction controller

--- src/controllers/transactionController.ts
+++ src/controllers/transactionController.ts
@@ -1,1 +1,42 @@
-import * as mongoose from "mongoose";
+import { TransactionModel } from "../models/transactionModel";
+
+export class TransactionController {
+  public getTransactionWithHandle(handle: String) {
+    TransactionModel.findOne(
+      { "handles.transaction.value": handle },
+      (err, transaction) => {
+        if (err) {
+          return err;
+        } else {
+          return transaction;
+        }
+      }
+    );
+  }
+
+  public getTransactionByInteractId(id: String) {
+    TransactionModel.findOne(
+      { "interact.interact_id": id },
+      (err, transaction) => {
+        if (err) {
+          return err;
+        } else {
+          return transaction;
+        }
+      }
+    );
+  }
+
+  public getTransactionByUserCode(code: String) {
+    TransactionModel.findOne(
+      { "interact.user_code": code },
+      (err, transaction) => {
+        if (err) {
+          return err;
+        } else {
+          return transaction;
+        }
+      }
+    );
+  }
+}
