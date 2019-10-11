96aaf1bccf3ccb121f72cd92f71e4d6c77f15839
Jacky Yuan <jacky.yuan@securekey.com>
2019-10-11 11:46:24 -0400

    fix bug: react webpage one step behind

--- client/web/src/App.js
+++ client/web/src/App.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-unused-expressions */
-import React, { Component } from 'react';
-import './App.css';
-import * as axios from 'axios';
-import * as constants from './Constants.js';
-import TxComponent from './TxComponent';
+import React, { Component } from "react";
+import "./App.css";
+import * as axios from "axios";
+import * as constants from "./Constants.js";
+import TxComponent from "./TxComponent";
 
 class App extends Component {
   constructor(props) {
@@ -15,26 +15,27 @@ class App extends Component {
 
   componentDidMount() {
     this.loadPendingTransactions();
+    this.forceUpdate();
   }
 
   loadPendingTransactions() {
     let currentComponent = this;
-    axios.get('http://localhost:3001/pending').then(function(response) {
+    axios.get("http://localhost:3001/pending").then(function(response) {
       console.log(
-        'pending transaction response is: ' + JSON.stringify(response.data)
+        "pending transaction response is: " + JSON.stringify(response.data)
       );
       currentComponent.setState({ transactions: response.data });
     });
   }
 
   newRedirectTransaction() {
-    axios.post('http://localhost:3001/redirect').then(resp => {
+    axios.post("http://localhost:3001/redirect").then(resp => {
       this.loadPendingTransactions();
     });
   }
 
   newDeviceTransaction() {
-    axios.post('http://localhost:3001/device').then(resp => {
+    axios.post("http://localhost:3001/device").then(resp => {
       this.loadPendingTransactions();
     });
   }
@@ -45,7 +46,6 @@ class App extends Component {
         <TxComponent key={transaction.id} txObject={transaction} />
       ))
       .reverse(); // newest first
-
     return (
       <div>
         <p>OAuthXYZ Client</p>
